76a5b5020852f1cbfd67cf725d3e19e78c66c9a8
Juan Acevedo <juan.acevedo@finaktiva.com>
2026-01-09 13:11:38 -0500

    fix: env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
+/*Local*/
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3001/api',
+//   catApiKey: 'live_JBT0Ah0Nt12iyl2IpjQVLDWjcLk0GQwf4zI9wBMfmfejKmcC31mOJp5TsOUP'
+// };
+
+/*Produccion*/
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3001/api',
+  production: true,
+  apiUrl: 'https://back-thenical-xpert.onrender.com/api',
   catApiKey: 'live_JBT0Ah0Nt12iyl2IpjQVLDWjcLk0GQwf4zI9wBMfmfejKmcC31mOJp5TsOUP'
 };
